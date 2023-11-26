d48187e99cb288e49e9ebc711a7703d426220595
roslynpius <roslyn.pius@gmail.com>
2023-11-27 01:04:07 +0530

    Implemented USECASE 3: Finding max of 3 string values

--- src/MaxValue.java
+++ src/MaxValue.java
@@ -1,42 +1,49 @@
 /**
- * Class to find the maximum of three float values.
+ * Class to find the maximum of three string values.
  */
 public class MaxValue {
 
     /**
-     * Finds the maximum of three float values.
+     * Finds the maximum of three String values.
      *
-     * @param num1 The first float value.
-     * @param num2 The second float value.
-     * @param num3 The third float value.
-     * @return The maximum float value among the three.
+     * @param str1 The first String value.
+     * @param str2 The second String value.
+     * @param str3 The third String value.
+     * @return The maximum String value among the three.
      */
-    public static float findMax(float num1, float num2, float num3) {
-        return Math.max(Math.max(num1, num2), num3);
+    public static String findMax(String str1, String str2, String str3) {
+        String max=str1;
+
+        if(str2.compareTo(max)>0)
+            max=str2;
+        if(str3.compareTo(max)>0)
+            max=str3;
+
+        return max;
     }
 
     /**
-     * Tests the findMax method with three float values and prints the result.
+     * Tests the findMax method with three String values and prints the result.
      *
-     * @param num1 The first float value.
-     * @param num2 The second float value.
-     * @param num3 The third float value.
+     * @param str1 The first String value.
+     * @param str2 The second String value.
+     * @param str3 The third String value.
      */
-    public static void testMaxNumber(float num1, float num2, float num3) {
-        float maxNumber = findMax(num1, num2, num3);
-        System.out.println("Test Case: " + num1 + ", " + num2 + ", " + num3);
-        System.out.println("Maximum Number: " + maxNumber);
+    public static void testMaxNumber(String str1, String str2, String str3) {
+        String maxString = findMax(str1, str2, str3);
+        System.out.println("Test Case: " + str1 +", " + str2 + ", " + str3);
+        System.out.println("Maximum Number: " + maxString);
         System.out.println();
     }
 
     public static void main(String[] args) {
         // Test Case 1: Max at 1st position
-        testMaxNumber(5.5f, 3.3f, 2.3f);
+        testMaxNumber("Zebra","Lion","Deer");
 
         // Test Case 2: Max at 2nd position
-        testMaxNumber(5.1f, 9.3f, 6.3f);
+        testMaxNumber("Apple", "Peach", "Banana");
 
         // Test Case 3: Max at 3rd position
-        testMaxNumber(4.9f, 2.3f, 11.2f);
+        testMaxNumber("movies","books","songs");
     }
 }
