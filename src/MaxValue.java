4a43ffcab1ef5962168846e1681b0cb99d3d789d
roslynpius <roslyn.pius@gmail.com>
2023-11-27 01:19:58 +0530

    Implemented Refactor 1: One Generic Method and find the maximum

--- src/MaxValue.java
+++ src/MaxValue.java
@@ -1,49 +1,50 @@
 /**
- * Class to find the maximum of three string values.
+ * Class to find the maximum of three comparable objects.
  */
 public class MaxValue {
 
     /**
-     * Finds the maximum of three String values.
+     * Finds the maximum of three comparable objects.
      *
-     * @param str1 The first String value.
-     * @param str2 The second String value.
-     * @param str3 The third String value.
-     * @return The maximum String value among the three.
+     * @param value1 The first object.
+     * @param value2 The second object.
+     * @param value3 The third object.
+     * @return The maximum value among the three.
      */
-    public static String findMax(String str1, String str2, String str3) {
-        String max=str1;
+    public static <T extends Comparable<T>> T findMax(T value1, T value2, T value3) {
+        T max=value1;
 
-        if(str2.compareTo(max)>0)
-            max=str2;
-        if(str3.compareTo(max)>0)
-            max=str3;
+        if(value2.compareTo(max)>0)
+            max=value2;
+        if(value3.compareTo(max)>0)
+            max=value3;
 
         return max;
     }
 
     /**
-     * Tests the findMax method with three String values and prints the result.
+     * Tests the findMax method with three comparable objects and prints the result.
      *
-     * @param str1 The first String value.
-     * @param str2 The second String value.
-     * @param str3 The third String value.
+     * @param value1 The first object.
+     * @param value2 The second object.
+     * @param value3 The third object.
+     * @param <T>  The type of the objects, must extend Comparable.
      */
-    public static void testMaxNumber(String str1, String str2, String str3) {
-        String maxString = findMax(str1, str2, str3);
-        System.out.println("Test Case: " + str1 +", " + str2 + ", " + str3);
-        System.out.println("Maximum Number: " + maxString);
+    public static <T extends Comparable<T>> void testMaxValue(T value1, T value2, T value3) {
+        T maxVal = findMax(value1, value2, value3);
+        System.out.println("Test Case: " + value1 +", " + value2 + ", " + value3);
+        System.out.println("Maximum Value: " + maxVal);
         System.out.println();
     }
 
     public static void main(String[] args) {
         // Test Case 1: Max at 1st position
-        testMaxNumber("Zebra","Lion","Deer");
+        testMaxValue(5,4,3);
 
         // Test Case 2: Max at 2nd position
-        testMaxNumber("Apple", "Peach", "Banana");
+        testMaxValue(3.3f,6.1f,1.2f);
 
         // Test Case 3: Max at 3rd position
-        testMaxNumber("movies","books","songs");
+        testMaxValue("movies","books","songs");
     }
 }
